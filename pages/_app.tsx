d9f7d519ae190315c5f27b8cb630a73e4979a2e0
LaurenceLama <laurencelamafse@gmail.com>
2023-07-18 22:20:14 +0800

    Build complete, pending debug for icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import type { AppProps } from 'next/app'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import Header from '../components/Header';
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
+import { useEffect, useState } from "react";
+import { Circle } from "better-react-spinkit";
+
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -22,15 +24,62 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
-      <Header />
-      <Navbar />
-      <Footer />
+      {loading ? (
+        <AppLoading>
+          <AppLoadingContents>
+            <img
+              src="https://download.logo.wine/logo/Star_Wars/Star_Wars-Logo.wine.png"
+              alt=""
+            />
+            <Circle color="#edec51" size={80} />
+          </AppLoadingContents>
+        </AppLoading>
+      ) : (
+        <>
+          <Header />
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    object-fit: contain;
+    height: 150px;
+    margin-bottom: 20px;
+    filter: brightness(0) invert(1);
+  }
+`;
